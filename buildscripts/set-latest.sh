da74879dfe728a04a013a0f8847aca70f5430d46
Madhu <madhu.ravi@aptible.com>
2022-05-17 11:08:44 -0400

    typo on buildscript when renamed missed

--- buildscripts/set-latest.sh
+++ buildscripts/set-latest.sh
@@ -21,7 +21,7 @@ rename_file_copy_to_latest() {
         echo "Found file that will not be renamed: $1"
     fi
 }
-export -f rename_file
+export -f rename_file_copy_to_latest
 
 cp -R pkg "deploy/${TRAVIS_REPO_SLUG}/latest/"
 find "deploy/${TRAVIS_REPO_SLUG}/latest" -name "aptible-toolbelt*" | xargs bash -c 'for arg; do rename_file_copy_to_latest "$arg"; done'
