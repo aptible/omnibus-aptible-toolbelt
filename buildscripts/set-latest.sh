d7a36d4960bc17a422a25359d624b6b93a632fa1
Madhu <madhu.ravi@aptible.com>
2022-05-17 11:01:37 -0400

    Multiple commits here from testing on Travis CI
    * using buildscript with bash
    * testing with xargs+moving complexity to shell script
    * accounted for .metadata.json with wildcard only on prefix
    * updated function name to be clearer
    * updated notations and justification

---
+++ buildscripts/set-latest.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+
+# this file is needed because the version is set by: `"#{semver}~#{platform}.#{platform_version}"`
+# in `util/version.rb`, which contains a latest timestamp by default. this timestamp is generated in
+# the build and as such is not nown ahead of time. 
+
+# this script just searches for the latest build by file extension, and makes a copy of it as 'latest'
+# for use in s3. ideal replacement to this issue would be to use releases/builds in github itself
+
+rename_file_copy_to_latest() {
+    # drops all the stuff that's usually caught by the wildcard in the find function "*" and replaces with `latest``
+    if [[ "$1" == *".metadata.json" ]]; then
+        # moves metadata file with latest suffix
+        mv "$1" "deploy/${TRAVIS_REPO_SLUG}/latest/aptible-toolbelt-latest_${DOCKER_TAG}_${TRAVIS_CPU_ARCH}${PKG_FILE_EXT}.metadata.json"
+    elif [[ "$1" == *"${PKG_FILE_EXT}" ]]; then
+        # moves versioned file (rpm/deb) with latest suffix
+        mv "$1" "deploy/${TRAVIS_REPO_SLUG}/latest/aptible-toolbelt-latest_${DOCKER_TAG}_${TRAVIS_CPU_ARCH}${PKG_FILE_EXT}"
+    else
+        echo "Found file that will not be renamed: $1"
+    fi
+}
+export -f rename_file
+
+cp -R pkg "deploy/${TRAVIS_REPO_SLUG}/latest/"
+find "deploy/${TRAVIS_REPO_SLUG}/latest" -name "aptible-toolbelt*" | xargs bash -c 'for arg; do rename_file_copy_to_latest "$arg"; done'
